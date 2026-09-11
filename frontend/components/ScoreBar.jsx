5b476e172e5b5b31701c02b53feb80893d76f1e9
KANHAIYA <kanhaiyamm55@gmail.com>
2026-09-11 08:20:34 +0530

    Delete frontend/components directory

--- frontend/components/ScoreBar.jsx
+++
@@ -1,1 +0,0 @@
-import React from 'react'; export default function ScoreBar({score}){return <div className="score"><strong>{score}/100</strong><div className="bar"><i style={{width:`${score}%`}}/></div></div>}
